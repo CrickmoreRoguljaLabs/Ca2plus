a5d32cf2f8cdc61040c898857bb71defb2c86bf4
xzhang03 <x.stephen.zhang@gmail.com>
2015-01-01 14:44:20 -0500

    Changes

---
+++ quickcal.m
@@ -0,0 +1,21 @@
+[filename, pathname] = uigetfile('*.tif');
+addpath(pathname)
+fullfilename = fullfile(pathname,filename);
+
+initialim = imread(fullfilename,1);
+%imshow(mat2gray(initialim))
+
+userpoly = getpoly(mat2gray(initialim));
+
+area = sum(userpoly(:));
+
+nframes = 60;
+
+intensityvec = zeros(nframes,1);
+
+for i=1:nframes
+    tempim = imread(fullfilename,i);
+    intensityvec(i) = mean(tempim(userpoly));
+end
+
+plot(intensityvec)
